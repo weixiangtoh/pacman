e87ddb19a60c3966377ad43347221131d3b0db42
weixiangtoh <weixiangtoh982@gmail.com>
2020-05-06 23:26:51 +0800

    Update maze.php

--- maze.php
+++ maze.php
@@ -10,7 +10,7 @@
 
 <html>
 	<head>
-		PHP Pacman Maze
+		<title>PHP Pacman Maze</title>
 		<script type="text/javascript" src="animation.js"></script>
 		<link rel="stylesheet" type="text/css" href="maze.css"/> 
 	</head>
